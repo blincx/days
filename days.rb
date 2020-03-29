a37e8ad7beaaa6697120936636e47c1192a28a9b
Eric Blincow <eblincow@barracuda.com>
2020-03-28 18:52:35 -0700

    yay

--- days.rb
+++ days.rb
@@ -1,18 +1,10 @@
 #/usr/bin/env ruby
 # Get days since born
 
-# example days.ini file:
-#[Birthday]
-#Birthday=1982/09/02
-
-require 'inifile'
 require 'pp'
 require 'date'
 
-# read in ini file
-file = IniFile.load('./days.ini')
-data = file["Birthday"]
-birthday = data["Birthday"]
+birthday = "1984/07/28"
 
 birthDate = Date.parse(birthday)
 now = DateTime.now
